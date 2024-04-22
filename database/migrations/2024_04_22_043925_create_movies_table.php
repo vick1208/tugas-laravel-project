c160fd6a2f9934893484553fc01dff1b48e95246
Vicky AS <vickyhuang808@gmail.com>
2024-04-22 11:43:09 +0700

    tugas 14 membuat migration

---
+++ database/migrations/2024_04_22_043925_create_movies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('summary')->nullable();
+            $table->integer('year');
+            $table->string('poster')->nullable();
+            $table->unsignedBigInteger('genre_id');
+            $table->timestamps();
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
